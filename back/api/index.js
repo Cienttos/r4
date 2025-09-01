987ce392b69173587fd9c07c6cc7785266f847ad
Cienttos <cientosoficial@gmail.com>
2025-09-01 02:27:41 -0300

    ser

--- back/api/index.js
+++ back/api/index.js
@@ -5,7 +5,6 @@ import dataRoutes from '../routes/dataRoutes.js';
 import errorHandler from '../utils/errorHandler.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
@@ -14,15 +13,11 @@ app.use(cors({
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', (req, res, next) => { console.log('Auth route hit'); next(); }, authRoutes);
-app.use('/api/data', (req, res, next) => { console.log('Data route hit'); next(); }, dataRoutes); // All data-related routes will be under /api/data/:tableName
+app.use('/api/auth', authRoutes);
+app.use('/api/data', dataRoutes);
 
 // Error handling middleware
 app.use(errorHandler);
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  console.log(`Auth API at http://localhost:${PORT}/api/auth`);
-  console.log(`Data API at http://localhost:${PORT}/api/data/<table_name>`);
-});
+// Export as a Vercel Serverless Function
+export default app;
